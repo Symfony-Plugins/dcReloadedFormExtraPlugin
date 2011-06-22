123d732fa261877a7aae8ea6163c47392dc00b80
pmacadden <pmacadden@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2011-06-22 18:55:23 -0500

    added pm forms
    git-svn-id: http://svn.symfony-project.com/plugins/dcReloadedFormExtraPlugin/trunk@32665 ee427ae8-e902-0410-961c-c3ed070cd9f9

---
+++ lib/form/pmWidgetFactory.class.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * @author Patricio Mac Adden <pmacadden@cespi.unlp.edu.ar>
+ */
+class pmWidgetFactory
+{
+  public static function replaceWidgets(sfForm $form)
+  {
+    foreach ($form->getWidgetSchema()->getFields() as $name => $widget)
+    {
+      if ($widget instanceof sfWidgetFormDate)
+      {
+        $form->setWidget($name, self::getDateWidget());
+      }
+      elseif ($widget instanceof sfWidgetFormFilterDate)
+      {
+        $form->getWidget($name)->setOption("from_date", self::getDateWidget(array("use_own_help" => false)));
+        $form->getWidget($name)->setOption("to_date", self::getDateWidget(array("use_own_help" => false)));
+        $form->getWidget($name)->setOption("template", __("from %from_date% to %to_date%"));
+      }
+      
+      if ($name == "attachment")
+      {
+        $form->setWidget($name, new sfWidgetFormInputFile());
+      }
+      elseif ($name == "created_by" || $name == "updated_by")
+      {
+        $form->setWidget($name, new sfWidgetFormInputHidden());
+      }
+      elseif ($name == "password")
+      {
+        $form->setWidget($name, new sfWidgetFormInputPassword());
+      }
+    }
+  }
+  
+  public static function getDateWidget($options = array())
+  {
+    return new mtWidgetFormInputDate($options);
+  }
+}
